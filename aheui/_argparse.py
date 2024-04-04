868e7e6de6ed699fe632215c58b9f1bbe94bf13d
Jeong, YunWon <69878+youknowone@users.noreply.github.com>
2024-04-05 01:01:05 +0900

    WarningPool 추가 (#34)

--- aheui/_argparse.py
+++ aheui/_argparse.py
@@ -157,5 +157,6 @@ parser.add_argument('--output', '-o', default='', description='Output file. Defa
 """)
 parser.add_argument('--cmd', '-c', default='', description='Program passed in as string')
 parser.add_argument('--no-c', '--no-c', narg='0', default='no', description='Do not generate `.aheuic` file automatically.', full_description='\tWhat is .aheuic? https://github.com/aheui/snippets/commit/cbb5a12e7cd2db771538ab28dfbc9ad1ada86f35\n')
+parser.add_argument('--warning-limit', '--warning-limit', default='3', description='Set repetitive warning limit. 0 means no warning. -1 means no limit.')
 parser.add_argument('--version', '-v', narg='-1', default='no', description='Show program version', message=VERSION)
 parser.add_argument('--help', '-h', narg='-1', default='no', description='Show this help text')
---
+++ aheui/warning.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+import os
+from aheui._compat import jit
+
+
+class Warning(object):
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+
+    def format(self, *args):
+        return self.message % args
+    
+
+WARNING_LIST = [
+    Warning(b'write-utf8-range', b'[Warning:UndefinedBehavior:write-utf8-range] value %x is out of unicode codepoint range.'),
+]
+
+
+class WarningPool(object):
+    def __init__(self):
+        self.limit = -1
+        self.warnings = {}
+        self.counters = {}
+        for w in WARNING_LIST:
+            self.warnings[w.name] = w
+            self.counters[w.name] = 0
+
+    @jit.dont_look_inside
+    def warn(self, name, *args):
+        warning = self.warnings[name]
+        if self.limit != -1 and self.limit <= self.counters[name]:
+            return
+        self.counters[name] = self.counters[name] + 1
+        os.write(2, warning.format(*args))
+        os.write(2, b'\n')
+        if self.limit != -1 and self.limit <= self.counters[name]:
+            os.write(2, b"[Warning:Meta] The warning '%s' has reached the limit %d and will be suppressed\n" % (warning.name, self.limit))
